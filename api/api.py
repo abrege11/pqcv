d8bb16f235616d7b0d9fed440e8656e7e0a3fe3d
abe brege <bregeabe@gmail.com>
2025-07-20 01:06:51 -0400

    connect febe

--- api/api.py
+++ api/api.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/api/generateInstructions")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/api/generateInstructions", methods=["POST"])
+def generateInstructions():
+    data = request.get_json()
+    print("Received data:", data)
+    return {"message": "Instructions received"}
--- src/services/channel.service.js
+++ src/services/channel.service.js
@@ -1,15 +1,18 @@
 import axios from 'axios';
 
 export const generateInstructions = async (data) => {
-//   try {
-//     const response = await axios.post('/api/generateInstructions', data, {
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//     });
-//     console.log('Data submitted successfully:', response.data);
-//   } catch (error) {
-//     console.error('Error submitting data:', error);
-//   }
-  console.log("here")
+  try {
+    const response = await axios.post(
+      'http://localhost:8080/api/generateInstructions',
+      data,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    console.log('Data submitted successfully:', response.data);
+  } catch (error) {
+    console.error('Error submitting data:', error);
+  }
 };
