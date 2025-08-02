0234fcd6c8f5284fc70b539fb4116015f88e8f1d
abe brege <bregeabe@gmail.com>
2025-08-02 16:26:32 -0400

    simulating circuit and sending back to FE

--- api/controllers/processor.py
+++ api/controllers/processor.py
@@ -1,17 +1,28 @@
 from qiskit import QuantumCircuit
+from qiskit.quantum_info import Statevector
 from ..helpers import serializeGates
 import matplotlib
 matplotlib.use('Agg')
-import matplotlib.pyplot as plt
 
 def delegate(channels):
     registers = len(channels)
     circuitInstructions = serializeGates(channels)
     qc = QuantumCircuit(registers)
-    instructions = processCircuit(qc, circuitInstructions)
+    instructions = getInstructions(circuitInstructions, registers)
     return {"message": instructions}
 
-def processCircuit(qc, circuitInstructions):
+def getInstructions(circuitInstructions, numQubits):
+    state = Statevector.from_int(0, dims=[2] * numQubits)
+    res=[]
+    for qubit, gates in circuitInstructions.items():
+        for gate in gates:
+            qc = QuantumCircuit(numQubits)
+            getattr(qc, gate)(int(qubit))
+            state = state.evolve(qc)
+            res.append([{"real": c.real, "imag": c.imag} for c in state.data])
+    return res
+
+def getCircuitPNG(qc, circuitInstructions):
     for qubit, gates in circuitInstructions.items():
         for gate in gates:
             gateMethod = getattr(qc, gate)
